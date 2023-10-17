845f2b9aa2e880c9d63fac7e7d6f13a8e733cf46
fazil <fazifazil160160@gmail.com>
2023-10-17 17:59:06 +0530

    registration is working (customer, user)

---
+++ src/pages/Customer/CustomerSignuPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CustomerRegister from '../../components/Customer/login/CustomerRegister'
+
+function CustomerSignuPage() {
+  return (
+    <div>
+      <CustomerRegister/>
+    </div>
+  )
+}
+
+export default CustomerSignuPage
--- src/routes/Customer.jsx
+++ src/routes/Customer.jsx
@@ -4,12 +4,13 @@ import PrivateRoutes from '../ProtectedRoutes/PrivteRoutes'
 import CustomerProtected from '../ProtectedRoutes/CustomerProtected'
 import CustomerHomePage from '../pages/Customer/CustomerHomePage'
 import CustomerLoginPage from '../pages/Customer/CustomerLoginPage'
+import CustomerSignuPage from '../pages/Customer/CustomerSignuPage'
 
 function CustomerRoutes() {
   return (
     <Routes>
         <Route element={<PrivateRoutes/>}>
-            {/* <Route path='/signup' element={<CompanyRegister/>} /> */}
+            <Route path='/signup' element={<CustomerSignuPage/>} />
             <Route path='/login' element={<CustomerLoginPage/>} />
         </Route>
 
