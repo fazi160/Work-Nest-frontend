07a9342bdc81467fbd892dab4f985a8ce10c5f73
fazil <fazifazil160160@gmail.com>
2023-12-08 09:41:15 +0530

    sample

--- src/App.jsx
+++ src/App.jsx
@@ -8,79 +8,79 @@ import axios from "axios";
 import { wsApiUrl } from "./constants/constants";
 
 function App() {
-  const [notifications, setNotifications] = useState([]);
-  const [socket, setSocket] = useState(null);
+  // const [notifications, setNotifications] = useState([]);
+  // const [socket, setSocket] = useState(null);
 
-  useEffect(() => {
+  // useEffect(() => {
 
   
-    const wsUrl = `${wsApiUrl}/ws/notification/`;
+  //   const wsUrl = `${wsApiUrl}/ws/notification/`;
   
-    const connectWebSocket = () => {
-      const client = new W3CWebSocket(wsUrl);
+  //   const connectWebSocket = () => {
+  //     const client = new W3CWebSocket(wsUrl);
   
-      client.onopen = (e) => {
-        console.log("WebSocket connection opened:", e);
-      };
+  //     client.onopen = (e) => {
+  //       console.log("WebSocket connection opened:", e);
+  //     };
   
-      client.onclose = (e) => {
-        console.log("WebSocket connection closed:", e);
-        setTimeout(connectWebSocket, 15000);
-      };
+  //     client.onclose = (e) => {
+  //       console.log("WebSocket connection closed:", e);
+  //       setTimeout(connectWebSocket, 15000);
+  //     };
   
-      client.onerror = (e) => {
-        console.error("WebSocket connection error:", e);
-      };
+  //     client.onerror = (e) => {
+  //       console.error("WebSocket connection error:", e);
+  //     };
   
-      client.onmessage = (message) => {
+  //     client.onmessage = (message) => {
         
-        const data = JSON.parse(message.data);
-        setNotifications((prevNotifications) => [...prevNotifications, data]);
-        showNotification(data.message);
-      };
+  //       const data = JSON.parse(message.data);
+  //       setNotifications((prevNotifications) => [...prevNotifications, data]);
+  //       showNotification(data.message);
+  //     };
   
-      return () => {
-        console.log("Cleaning up WebSocket connection...");
-        client.close();
-      };
-    };
+  //     return () => {
+  //       console.log("Cleaning up WebSocket connection...");
+  //       client.close();
+  //     };
+  //   };
   
-    // Start the initial connection
-    connectWebSocket();
-  }, []);
+  //   // Start the initial connection
+  //   connectWebSocket();
+  // }, []);
   
 
-  const showNotification = (message) => {
-    // Check if the browser supports notifications
-    if (!("Notification" in window)) {
-      console.log("This browser does not support desktop notification");
-      return;
-    }
+  // const showNotification = (message) => {
+  //   // Check if the browser supports notifications
+  //   if (!("Notification" in window)) {
+  //     console.log("This browser does not support desktop notification");
+  //     return;
+  //   }
 
-    // Check if the user has granted permission for notifications
-    if (Notification.permission === "granted") {
-      // Create and show the notification
-      const notification = new Notification("New Notification", {
-        body: message,
-      });
+  //   // Check if the user has granted permission for notifications
+  //   if (Notification.permission === "granted") {
+  //     // Create and show the notification
+  //     const notification = new Notification("New Notification", {
+  //       body: message,
+  //     });
 
-      // Close the notification after a few seconds (adjust as needed)
-      setTimeout(notification.close.bind(notification), 50000);
-    } else if (Notification.permission !== "denied") {
-      // Request permission from the user
-      Notification.requestPermission().then((permission) => {
-        if (permission === "granted") {
-          // Create and show the notification
-          const notification = new Notification("New Notification", {
-            body: message,
-          });
+  //     // Close the notification after a few seconds (adjust as needed)
+  //     setTimeout(notification.close.bind(notification), 50000);
+  //   } else if (Notification.permission !== "denied") {
+  //     // Request permission from the user
+  //     Notification.requestPermission().then((permission) => {
+  //       if (permission === "granted") {
+  //         // Create and show the notification
+  //         const notification = new Notification("New Notification", {
+  //           body: message,
+  //         });
 
-          // Close the notification after a few seconds (adjust as needed)
-          setTimeout(notification.close.bind(notification), 5000);
-        }
-      });
-    }
-  };
+  //         // Close the notification after a few seconds (adjust as needed)
+  //         setTimeout(notification.close.bind(notification), 5000);
+  //       }
+  //     });
+  //   }
+  // };
 
   return (
     <div>
