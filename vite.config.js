24921e5f1277455955d79e3677d486891a5650e6
fazil <fazifazil160160@gmail.com>
2023-12-08 21:00:10 +0530

    back to old

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/Work-Nest-frontend/"
+  server: {
+    host: '0.0.0.0',
+  },
 })
