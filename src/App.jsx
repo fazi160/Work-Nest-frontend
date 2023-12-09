33acb71cf82c9eb1d2e5903bb1432cf09b800c2a
fazil <fazifazil160160@gmail.com>
2023-12-09 10:29:38 +0530

    link added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import AdminRouter from "./routes/admin";
 import CustomerRoutes from "./routes/Customer";
 import UserRoutes from "./routes/User";
