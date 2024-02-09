77851d719438f32a7b93605022c10650a32dc589
fazil <fazifazil160160@gmail.com>
2024-02-09 14:23:34 +0530

    removed restrict mode

--- src/main.jsx
+++ src/main.jsx
@@ -9,9 +9,9 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
 
     <GoogleOAuthProvider clientId="208811582234-eog42g3j4cuh7g5ufphp47smbr6c5vv9.apps.googleusercontent.com">
-      <React.StrictMode>
+      
         <App />
-      </React.StrictMode>
+      
     </GoogleOAuthProvider>
 
 );
